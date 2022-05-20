24da4d0ef6600e16c73c2883be63fea06008d3f5
FieryInferno <bagassetia271@gmail.com>
2022-05-20 15:40:07 +0700

    Sort by latest

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -15,10 +15,11 @@ class Welcome extends CI_Controller {
   public function shop()
   {
     $kategori = $this->input->get('kategori');
+    $sortBy   = $this->input->get('sortBy');
 
 		$this->load->view('shop', [
       'kategori'  => $this->KategoriModel->getAll(),
-      'produk'    => $this->ProdukModel->getAll($kategori),
+      'produk'    => $this->ProdukModel->getAll($kategori, $sortBy),
       'type'      => 'shop',
     ]); 
   }
--- application/models/ProdukModel.php
+++ application/models/ProdukModel.php
@@ -8,14 +8,18 @@ class ProdukModel extends CI_Model {
     $this->db->insert('produk', $data);
 	}
 
-  public function getAll($id_kategori)
+  public function getAll($id_kategori = null, $sortBy = null)
   {
     $this->db->join('kategori', 'produk.kategori_id = kategori.id_kategori');
 
     if ($id_kategori) {
       $this->db->where('produk.kategori_id', $id_kategori);
     }
-    
+
+    if ($sortBy) {
+      $this->db->order_by('created_at', 'desc');
+    }
+
     return $this->db->get('produk')->result_array();
   }
 
