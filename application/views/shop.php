3870a7c5085771a0e924b6c66c60d7d2e0a7f2f6
FieryInferno <bagassetia271@gmail.com>
2022-06-10 17:21:18 +0700

    Fix shop

--- application/views/shop.php
+++ application/views/shop.php
@@ -210,7 +210,7 @@
             <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
               <div class="card product-item border-0 mb-4">
                 <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                  <img class="img-fluid w-100" src="<?= base_url(); ?>assets/image/<?= $key['image']; ?>" alt="">
+                  <img class="img-fluid w-100" src="<?= count($key['image']) > 0 ? base_url('assets/image/' . $key['image'][0]['gambar']) : ''; ?>" alt="">
                 </div>
                 <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
                   <h6 class="text-truncate mb-3"><?= $key['nama_produk']; ?></h6>
